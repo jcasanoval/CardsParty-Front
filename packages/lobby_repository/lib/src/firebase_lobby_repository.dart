12dfeb9b4e428f24b6110a2d58ad1434f52ef5a7
Juan Casanova <j.casanoval@hotmail.com>
2023-08-28 17:13:56 -0300

    feat: implement basic createLobby

--- packages/lobby_repository/lib/src/firebase_lobby_repository.dart
+++ packages/lobby_repository/lib/src/firebase_lobby_repository.dart
@@ -3,7 +3,7 @@ import 'package:lobby_repository/lobby_repository.dart';
 
 /// Provides an instance of the lobbies collection
 CollectionReference<Lobby> _usersCollectionInstance() {
-  return FirebaseFirestore.instance.collection('users').withConverter<Lobby>(
+  return FirebaseFirestore.instance.collection('lobbies').withConverter<Lobby>(
         fromFirestore: (snapshot, _) => Lobby.fromJson(snapshot.data()!),
         toFirestore: (lobby, _) => lobby.toJson(),
       );
@@ -21,9 +21,11 @@ class FirebaseLobbyRepository extends LobbyRepositoryContract {
   final CollectionReference<Lobby> _lobbyCollection;
 
   @override
-  Future<Lobby> createLobby() {
-    // TODO: implement createLobby
-    throw UnimplementedError();
+  Future<Lobby> createLobby() async {
+    final doc = _lobbyCollection.doc();
+    final lobby = Lobby.withRandomCode(id: doc.id);
+    await doc.set(lobby);
+    return lobby;
   }
 
   @override
--- packages/lobby_repository/lib/src/models/lobby.dart
+++ packages/lobby_repository/lib/src/models/lobby.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 const _kId = 'id';
 const _kGameCode = 'gameCode';
 
@@ -9,6 +11,24 @@ class Lobby {
 
   final String id;
   final String gameCode;
+
+  Lobby.withRandomCode({
+    required String id,
+  }) : this(
+          id: id,
+          gameCode: _generateRandomCode(),
+        );
+
+  static String _generateRandomCode() {
+    final random = Random();
+    final asciiA = 'A'.codeUnitAt(0);
+    final asciiZ = 'Z'.codeUnitAt(0);
+    final codeUnits = List.generate(4, (index) {
+      return random.nextInt(asciiZ - asciiA + 1) + asciiA;
+    });
+
+    return String.fromCharCodes(codeUnits);
+  }
 
   Lobby.fromJson(Map<String, Object?> json)
       : this(
