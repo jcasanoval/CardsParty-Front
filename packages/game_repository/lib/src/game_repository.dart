468505009b681eefe76134b866fbccd7bbe7f182
Juan Casanova <j.casanoval@hotmail.com>
2023-08-29 15:48:13 -0300

    chore: initialize game repository

---
+++ packages/game_repository/lib/src/game_repository.dart
@@ -0,0 +1,7 @@
+/// {@template game_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class GameRepository {
+  /// {@macro game_repository}
+  const GameRepository();
+}
