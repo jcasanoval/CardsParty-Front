17098ea48a30b2d09fa20dcc81a6fe7fab35f48f
Juan Casanova <j.casanoval@hotmail.com>
2023-08-29 11:50:12 -0300

    chore: export lobby exceptions

--- packages/lobby_repository/lib/lobby_repository.dart
+++ packages/lobby_repository/lib/lobby_repository.dart
@@ -2,5 +2,6 @@
 library lobby_repository;
 
 export 'src/contracts/contracts.dart';
+export 'src/exceptions/exceptions.dart';
 export 'src/firebase_lobby_repository.dart';
 export 'src/models/models.dart';
