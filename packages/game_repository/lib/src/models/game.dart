a5e72dfd70bf145bc4ba2c37844186f27f7595d6
Juan Casanova <j.casanoval@hotmail.com>
2023-08-29 17:15:41 -0300

    feat: Basic game mapping methods

--- packages/game_repository/lib/src/models/game.dart
+++ packages/game_repository/lib/src/models/game.dart
@@ -1,7 +1,17 @@
+const _kId = 'id';
+
 class Game {
   Game({
     required this.id,
   });
 
+  Game.fromJson(Map<String, dynamic> json) : this(id: json[_kId] as String);
+
   final String id;
+
+  Map<String, dynamic> toJson() {
+    return {
+      _kId: id,
+    };
+  }
 }
