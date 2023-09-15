3005fee669572ca048370340d40f9d15b1d209a7
Juan Casanova <j.casanoval@hotmail.com>
2023-09-14 22:37:16 -0300

    Feat: init package

---
+++ packages/rules_repository/lib/src/rules_repository.dart
@@ -0,0 +1,7 @@
+/// {@template rules_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class RulesRepository {
+  /// {@macro rules_repository}
+  const RulesRepository();
+}
