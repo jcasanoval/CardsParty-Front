b37241bfa5813f1715cf719f1dbc16c94f8a49fe
Juan Casanova <j.casanoval@hotmail.com>
2023-10-16 19:15:44 -0300

    Import and format fixes

--- packages/rules_repository/lib/rules_repository.dart
+++ packages/rules_repository/lib/rules_repository.dart
@@ -2,7 +2,6 @@
 library rules_repository;
 
 export 'src/contracts/contracts.dart';
-export 'src/contracts/game_rule_contract.dart';
 export 'src/exceptions/exceptions.dart';
 export 'src/models/models.dart';
 export 'src/rules_repository.dart';
--- packages/rules_repository/lib/src/models/ui_element.dart
+++ packages/rules_repository/lib/src/models/ui_element.dart
@@ -1,5 +1,3 @@
-import '/Users/lalonsotomas/meli/CardsParty-Front/packages/game_repository/lib/game_repository.dart';
-
 part 'button.dart';
 
 sealed class UIElement {
