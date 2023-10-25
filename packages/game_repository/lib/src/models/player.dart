378e41fb524c7c8e7a6e8fef57b5b564f8977ec5
Juan Casanova <j.casanoval@hotmail.com>
2023-10-25 13:20:44 -0300

    Add serialization methods to player

--- packages/game_repository/lib/src/models/player.dart
+++ packages/game_repository/lib/src/models/player.dart
@@ -1,9 +1,19 @@
 class Player {
-  final String id;
-  final List<int?> cards; // Lista de cartas del jugador
-
   Player({
     required this.id,
     required this.cards,
   });
+
+  final String id;
+  final List<int?> cards; // Lista de cartas del jugador
+
+  //TODO(Lautaro77): implement fromJson
+  Player.fromJson(Map<String, dynamic> json)
+      : id = '',
+        cards = [];
+
+  //TODO(Lautaro77): implement toJson
+  Map<String, dynamic> toJson() {
+    return {};
+  }
 }
