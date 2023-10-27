abb355caa046ea554268eed5d605a2f092feab6d
Juan Casanova <j.casanoval@hotmail.com>
2023-10-27 15:57:31 -0300

    Add parameters to game and fix parsing

--- packages/game_repository/lib/src/models/game.dart
+++ packages/game_repository/lib/src/models/game.dart
@@ -1,34 +1,49 @@
 import 'package:game_repository/game_repository.dart';
+import 'package:lobby_repository/lobby_repository.dart';
 
 const _kId = 'id';
 const _kPlayers = 'players';
-const _KCustomParams = 'customParams';
+const _kCustomParams = 'customParams';
+const _kHostId = 'hostId';
 
 class Game {
   Game({
     required this.id,
-    // this.players = const [],
+    required this.players,
+    required this.hostId,
     this.customParams = const {},
   });
 
-  Game.fromJson(Map<dynamic, dynamic> json)
+  Game.fromJson(Map<String, dynamic> json)
       : this(
           id: json[_kId] as String,
-          // players: (json[_kPlayers] as List<Map<String, dynamic>>)
-          //     .map((playerJson) => Player.fromJson(playerJson))
-          //     .toList(),
-          customParams: json[_KCustomParams] as Map<dynamic, dynamic>? ?? {},
+          players: (json[_kPlayers] as List<Map<String, dynamic>>)
+              .map(Player.fromJson)
+              .toList(),
+          customParams: json.containsKey(_kCustomParams)
+              ? json[_kCustomParams] as Map<String, dynamic>
+              : {},
+          hostId: json[_kHostId] as String,
+        );
+
+  Game.fromLobby(Lobby lobby)
+      : this(
+          id: lobby.id,
+          players: lobby.players.map(Player.fromLobbyPlayer).toList(),
+          hostId: lobby.hostId,
         );
 
   final String id;
-  // final List<Player> players;
-  final Map<dynamic, dynamic> customParams;
+  final List<Player> players;
+  final String hostId;
+  final Map<String, dynamic> customParams;
 
-  Map<String, dynamic> toJson() {
+  Map<String, Object> toJson() {
     return {
       _kId: id,
-      // _kPlayers: players.map((player) => player.toJson()),
-      _KCustomParams: customParams,
+      _kPlayers: players.map((player) => player.toJson()).toList(),
+      _kCustomParams: customParams,
+      _kHostId: hostId,
     };
   }
 }
