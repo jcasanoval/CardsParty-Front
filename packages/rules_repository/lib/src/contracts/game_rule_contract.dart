2d4f8c9b073062a13b6134333836670b8d0332dc
Juan Casanova <j.casanoval@hotmail.com>
2023-11-16 12:36:26 -0300

    feat: made Game rule constructor const

--- packages/rules_repository/lib/src/contracts/game_rule_contract.dart
+++ packages/rules_repository/lib/src/contracts/game_rule_contract.dart
@@ -2,6 +2,8 @@ import 'package:game_repository/game_repository.dart';
 import 'package:rules_repository/rules_repository.dart';
 
 abstract class GameRuleContract {
+  const GameRuleContract();
+
   final int priority = 0;
 
   Game applyRule(int userId, Game gameState);
