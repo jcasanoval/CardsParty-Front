fcdfdcdcd5d960e7cf5471f2fbd092c87c4500e5
Juan Casanova <j.casanoval@hotmail.com>
2023-11-21 15:52:26 -0300

    feat: display correct players

--- lib/game_screen/widget/hands_layer/player_hands_layer.dart
+++ lib/game_screen/widget/hands_layer/player_hands_layer.dart
@@ -1,51 +1,66 @@
 import 'dart:math';
 
+import 'package:cards_party/app/app.dart';
 import 'package:cards_party/game_screen/game_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:game_repository/game_repository.dart';
 
 class PlayerHandsLayer extends StatelessWidget {
   const PlayerHandsLayer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final players =
-        (context.watch<GameCubit>().state as GameLoaded).game.players;
+    final players = sinkCurrentPlayerToBottom(
+      (context.watch<GameCubit>().state as GameLoaded).game.players,
+      context.read<AuthCubit>().currentUser.id,
+    );
 
     return Stack(
       fit: StackFit.expand,
       alignment: Alignment.center,
       children: [
-        // TODO(jcasanoval): Display correct players
         Positioned(
           bottom: 0,
           child: PlayerHand(
             player: players[0],
           ),
         ),
-        Positioned(
-          top: 0,
-          child: PlayerHand(
-            player: players[0],
-            rotation: pi,
-            rotateText: true,
+        if (players.length == 2 || players.length == 4)
+          Positioned(
+            top: 0,
+            child: PlayerHand(
+              player: players.length == 2 ? players[1] : players[2],
+              rotation: pi,
+              rotateText: true,
+            ),
           ),
-        ),
-        Positioned(
-          left: 0,
-          child: PlayerHand(
-            player: players[0],
-            rotation: pi / 2,
+        if (players.length == 3 || players.length == 4)
+          Positioned(
+            left: 0,
+            child: PlayerHand(
+              player: players.length == 3 ? players[2] : players[3],
+              rotation: pi / 2,
+            ),
           ),
-        ),
-        Positioned(
-          right: 0,
-          child: PlayerHand(
-            player: players[0],
-            rotation: pi * 3 / 2,
+        if (players.length == 3 || players.length == 4)
+          Positioned(
+            right: 0,
+            child: PlayerHand(
+              player: players[1],
+              rotation: pi * 3 / 2,
+            ),
           ),
-        ),
       ],
     );
+  }
+
+  List<Player> sinkCurrentPlayerToBottom(List<Player> players, String userId) {
+    final clonedPlayers = List<Player>.from(players);
+    while (clonedPlayers[0].id != userId) {
+      final currentPlayer = clonedPlayers.removeAt(0);
+      clonedPlayers.add(currentPlayer);
+    }
+    return clonedPlayers;
   }
 }
