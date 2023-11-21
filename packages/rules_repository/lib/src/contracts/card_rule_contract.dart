d24424f26c01c1511a8c4ac255d751a64d0a01d7
Juan Casanova <j.casanoval@hotmail.com>
2023-11-21 10:48:19 -0300

    chore: barrel export

--- packages/rules_repository/lib/src/contracts/card_rule_contract.dart
+++ packages/rules_repository/lib/src/contracts/card_rule_contract.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/widgets.dart';
 import 'package:game_repository/game_repository.dart';
 import 'package:rules_repository/rules_repository.dart';
-import 'package:rules_repository/src/contracts/rule_contract.dart';
-import 'package:rules_repository/src/models/card_action.dart';
 
-abstract class CardRuleContract extends RuleContract{
+abstract class CardRuleContract extends RuleContract {
   Game applyRule(String userId, Game gameState, Card card);
 
   CardAction? conditionMet(String userId, Game gameState, Card card);
