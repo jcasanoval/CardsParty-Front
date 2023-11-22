a83be2bc295c2f42901225a404529ec19936734b
Juan Casanova <j.casanoval@hotmail.com>
2023-11-22 17:22:43 -0300

    fix: add priority to gamerule

--- packages/rules_repository/lib/src/contracts/game_rule_contract.dart
+++ packages/rules_repository/lib/src/contracts/game_rule_contract.dart
@@ -1,10 +1,9 @@
 import 'package:game_repository/game_repository.dart';
 import 'package:rules_repository/rules_repository.dart';
-import 'package:rules_repository/src/contracts/rule_contract.dart';
 
 abstract class GameRuleContract extends RuleContract {
-  const GameRuleContract();
-  
+  const GameRuleContract({required super.priority});
+
   Game applyRule(String userId, Game gameState);
 
   UIElement? conditionMet(String userId, Game gameState);
