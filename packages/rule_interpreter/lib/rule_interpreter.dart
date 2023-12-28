c35419cd4eabbcea2440a631b1cc5cea8d4a2102
Juan Casanova <j.casanoval@hotmail.com>
2023-12-28 14:38:57 -0300

    chore: remove unused files, gitkeep

--- packages/rule_interpreter/lib/rule_interpreter.dart
+++ packages/rule_interpreter/lib/rule_interpreter.dart
@@ -1,4 +1,4 @@
 /// Provides logic for interpreting rules using a composite tree pattern
 library rule_interpreter;
 
-export 'src/rule_interpreter.dart';
+export 'src/contracts/contracts.dart';
--- packages/rule_interpreter/lib/src/rule_interpreter.dart
+++
@@ -1,7 +0,0 @@
-/// {@template rule_interpreter}
-/// Provides logic for interpreting rules using a composite tree pattern
-/// {@endtemplate}
-class RuleInterpreter {
-  /// {@macro rule_interpreter}
-  const RuleInterpreter();
-}
