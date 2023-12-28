98fef88b3b7da78bfd255d2a5f60698985bdfdbd
Juan Casanova <j.casanoval@hotmail.com>
2023-12-28 15:08:36 -0300

    chore: add const constructors to base classes

--- packages/rule_interpreter/lib/src/contracts/expression.dart
+++ packages/rule_interpreter/lib/src/contracts/expression.dart
@@ -1,7 +1,12 @@
 import 'package:game_repository/game_repository.dart';
 
+/// {@template expression}
 /// An Expression that can be evaluated.
+/// {@endtemplate}
 abstract class Expression {
+  /// {@macro expression}
+  const Expression();
+
   /// Evaluates the Expression and modifies the provided game and context.
   void evaluate(Game game, Map<String, dynamic> context);
 }
