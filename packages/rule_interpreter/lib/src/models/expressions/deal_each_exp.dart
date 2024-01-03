04afb1993d8080e5f80590fe093d2acddcbbd87d
Juan Casanova <j.casanoval@hotmail.com>
2024-01-03 20:34:59 -0300

    feat: deal each exp

---
+++ packages/rule_interpreter/lib/src/models/expressions/deal_each_exp.dart
@@ -0,0 +1,23 @@
+import 'package:game_repository/game_repository.dart';
+import 'package:rule_interpreter/rule_interpreter.dart';
+
+/// {@template deal_each_exp}
+/// An Expression that deals each player a set number of cards.
+/// {@endtemplate}
+class DealEachExp extends Expression {
+  /// {@macro deal_each_exp}
+  const DealEachExp({required this.amount});
+
+  /// The number of cards to deal each player.
+  final Statement<int> amount;
+
+  @override
+  void evaluate(Game game, String userId, Context context) {
+    final amount = this.amount.evaluate(game, userId, context);
+    for (final player in game.players) {
+      for (var i = 0; i < amount; i++) {
+        player.cards.add(game.deck.removeAt(0));
+      }
+    }
+  }
+}
