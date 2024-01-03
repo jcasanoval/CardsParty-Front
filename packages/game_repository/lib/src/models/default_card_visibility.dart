408571013a5edad3a2f8a4abc40c9ce63f74d1e9
Juan Casanova <j.casanoval@hotmail.com>
2024-01-03 19:09:02 -0300

    feat: create method

---
+++ packages/game_repository/lib/src/models/default_card_visibility.dart
@@ -0,0 +1,11 @@
+/// Defines the default visibility of cards in a game.
+enum DefaultCardVisibility {
+  /// Cards are only visible to the owner.
+  owner,
+
+  /// Cards are visible to all players.
+  all,
+
+  /// Cards are not visible to any player.
+  none,
+}
