21b005557df084a8133c2044826bf3f8dbfb76e3
Juan Casanova <j.casanoval@hotmail.com>
2024-01-03 19:41:14 -0300

    feat: if expression

---
+++ packages/rule_interpreter/lib/src/models/expressions/if_exp.dart
@@ -0,0 +1,32 @@
+import 'package:game_repository/game_repository.dart';
+import 'package:rule_interpreter/rule_interpreter.dart';
+
+/// {@template if_exp}
+/// An Expression that evaluates a condition and calls the then or otherwise
+/// {@endtemplate}
+class IfExp extends Expression {
+  /// {@macro if_exp}
+  const IfExp({
+    required this.condition,
+    required this.then,
+    this.otherwise,
+  });
+
+  /// The condition to evaluate.
+  final Statement<bool> condition;
+
+  /// The Expression to evaluate if the condition is true.
+  final Expression then;
+
+  /// The Expression to evaluate if the condition is false.
+  final Expression? otherwise;
+
+  @override
+  void evaluate(Game game, String userId, Context context) {
+    if (condition.evaluate(game, userId, context)) {
+      then.evaluate(game, userId, context);
+    } else {
+      otherwise?.evaluate(game, userId, context);
+    }
+  }
+}
