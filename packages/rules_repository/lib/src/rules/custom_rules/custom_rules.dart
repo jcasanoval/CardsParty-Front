c1ca998ed9f71925ec24f29a2c6d0c6f72d266dc
Juan Casanova <j.casanoval@hotmail.com>
2024-01-03 21:21:11 -0300

    chore: barrel files

---
+++ packages/rules_repository/lib/src/rules/custom_rules/custom_rules.dart
@@ -0,0 +1,2 @@
+export 'custom_card_rule.dart';
+export 'custom_game_rule.dart';
--- packages/rules_repository/lib/src/rules/rules.dart
+++ packages/rules_repository/lib/src/rules/rules.dart
@@ -1,2 +1,3 @@
 export 'chancho/chancho.dart';
+export 'custom_rules/custom_rules.dart';
 export 'discard_draw/discard_draw.dart';
