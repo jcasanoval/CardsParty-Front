d05f8bf815882adff3e1df9e6889d3ad80522714
Juan Casanova <j.casanoval@hotmail.com>
2024-01-03 21:23:26 -0300

    feat: first custom rule using interpreter

--- packages/rules_repository/lib/src/rules/chancho/deal_cards_rule.dart
+++ packages/rules_repository/lib/src/rules/chancho/deal_cards_rule.dart
@@ -1,27 +1,16 @@
 import 'package:game_repository/game_repository.dart';
+import 'package:rule_interpreter/rule_interpreter.dart';
 import 'package:rules_repository/rules_repository.dart';
 
 class DealCardsRule extends GameRuleContract {
-  const DealCardsRule() : super(priority: 0);
+  const DealCardsRule() : super(priority: 1);
 
   @override
   Game applyRule(String userId, Game gameState) {
-    final cards = <Card>[];
-    var i = 1;
     for (final player in gameState.players) {
-      cards
-        ..add(Card(number: i, suit: Suit.club))
-        ..add(Card(number: i, suit: Suit.diamond))
-        ..add(Card(number: i, suit: Suit.heart))
-        ..add(Card(number: i, suit: Suit.spade));
-      i++;
-    }
-
-    cards.shuffle();
-
-    for (final player in gameState.players) {
-      player.cards.addAll(cards.sublist(0, 4));
-      cards.removeRange(0, 4);
+      while (player.cards.length < 4) {
+        player.cards.add(gameState.deck.removeAt(0));
+      }
     }
 
     gameState.customParams['state'] = 'RoundInProgress';
@@ -45,3 +34,38 @@ class DealCardsRule extends GameRuleContract {
     return null;
   }
 }
+
+const dealCardsCustomRule = CustomGameRule(
+  priority: 1,
+  conditionMetExp: IfExp(
+    condition: BoolComparatorStm(
+      BoolComparator.equal,
+      VariableStm('game.state'),
+      LiteralStm('RoundStart'),
+    ),
+    then: IfExp(
+      condition: BoolComparatorStm(
+        BoolComparator.equal,
+        VariableStm('currentPlayer.id'),
+        VariableStm('game.hostId'),
+      ),
+      then: ReturnButtonExp(
+        buttonLabel: LiteralStm('Deal cards!'),
+        enabled: LiteralStm(true),
+        showButton: LiteralStm(true),
+        color: ButtonColor.yellow,
+        size: ButtonSize.medium,
+        type: ButtonType.rounded,
+      ),
+    ),
+  ),
+  applyRuleExp: BlockExp(
+    expressions: [
+      DealEachExp(amount: LiteralStm(4)),
+      SetVariableExp(
+        variableName: 'game.state',
+        value: LiteralStm('RoundInProgress'),
+      ),
+    ],
+  ),
+);
