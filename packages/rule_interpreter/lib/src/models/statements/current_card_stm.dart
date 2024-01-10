adb25dd6c547430f1a037fc1d311410d72c20b99
Juan Casanova <j.casanoval@hotmail.com>
2024-01-10 19:51:25 -0300

    feat: current card stm

---
+++ packages/rule_interpreter/lib/src/models/statements/current_card_stm.dart
@@ -0,0 +1,19 @@
+import 'package:game_repository/game_repository.dart';
+import 'package:rule_interpreter/rule_interpreter.dart';
+
+/// {@template current_card_stm}
+/// A Statement that returns the current card.
+/// {@endtemplate}
+class CurrentCardStm extends Statement<Card> {
+  /// {@macro current_card_stm}
+  const CurrentCardStm();
+
+  @override
+  Card evaluate(Game game, String userId, Context context, [Card? card]) {
+    if (card == null) {
+      throw Exception('Card is null');
+    }
+
+    return card;
+  }
+}
