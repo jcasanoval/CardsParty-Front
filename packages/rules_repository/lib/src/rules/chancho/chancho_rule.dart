5cf88df472cc8db663b1be64f50411cb114eb327
Juan Casanova <j.casanoval@hotmail.com>
2024-01-29 19:40:11 -0300

    feat: chancho rule code

--- packages/rules_repository/lib/src/rules/chancho/chancho_rule.dart
+++ packages/rules_repository/lib/src/rules/chancho/chancho_rule.dart
@@ -1,3 +1,4 @@
+import 'package:game_repository/game_repository.dart';
 import 'package:game_repository/src/models/game.dart';
 import 'package:rules_repository/rules_repository.dart';
 
@@ -6,8 +7,22 @@ class ChanchoRule extends GameRuleContract {
 
   @override
   Game applyRule(String userId, Game gameState) {
-    // TODO: implement applyRule
-    throw UnimplementedError();
+    final player =
+        gameState.players.firstWhere((player) => player.id == userId);
+    player.customParams['chancho'] = true;
+    final unchanchoed = gameState.players
+        .where((player) => player.customParams['chancho'] == null);
+    if (unchanchoed.length == 1) {
+      unchanchoed.first.score = unchanchoed.first.score! + 1;
+      for (final player in gameState.players) {
+        player.customParams['chancho'] = null;
+        gameState.deck.addAll(player.cards);
+        player.cards.clear();
+        gameState.customParams['state'] = 'RoundStart';
+      }
+      gameState.deck.shuffle();
+    }
+    return gameState;
   }
 
   @override
